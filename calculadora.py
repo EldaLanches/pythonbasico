07625f86bb37852b9ee22d4501179b817685f933
Eloá Casarotto <eloa.casarotto@escola.pr.gov.br>
2026-02-09 14:11:16 +0000

    aula1

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+num1 = int(input("digete o primeiro número"))
+num2 = int(input("digete o segundo número"))
+
+soma = num1+num2
+menos = num1-num2
+vezes = num1*num2
+divi = num1/num2
+
+print("A soma é : ",soma)
+print("A subtração é : ",menos)
+print("A soma é : ",soma)
+print("A soma é : ",soma)
